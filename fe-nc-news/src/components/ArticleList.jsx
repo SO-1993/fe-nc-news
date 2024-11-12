dfa57c1b96823c919e78d2261f3dc044b312773e
Scott <scottobrien93@outlook.com>
2024-11-12 15:32:05 +0000

    Created a responsive Header that is linked with App

---
+++ fe-nc-news/src/components/ArticleList.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+function ArticleList() {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+}
+
+export default Header;
